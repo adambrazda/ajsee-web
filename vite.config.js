14824bf25db2a1bb1512cbd5e2bed174bcade6f3
Brázda Adam <adam.brazda@grit.cz>
2025-08-17 19:17:36 +0200

    Optimalizace webu

--- vite.config.js
+++ vite.config.js
@@ -1,38 +1,64 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import { resolve } from 'path'
+import { existsSync } from 'fs'
 import { viteStaticCopy } from 'vite-plugin-static-copy'
 
+// Helper: přidej vstup jen když existuje
+function addIfExists(obj, key, filepath) {
+  if (existsSync(filepath)) obj[key] = filepath
+  return obj
+}
+
 export default defineConfig({
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, 'src'),
+      '@styles': resolve(__dirname, 'src/styles'),
+    },
+  },
+
+  // ⚠️ Záměrně bez css.preprocessorOptions.scss.additionalData
+  // -> používáme explicitní `@use` v SCSS souborech
+
   build: {
     rollupOptions: {
-      input: {
-        main: resolve(__dirname, 'index.html'),
-        about: resolve(__dirname, 'about.html'),
-        events: resolve(__dirname, 'events.html'),
-        partners: resolve(__dirname, 'partners.html'),
-        thankyou: resolve(__dirname, 'thank-you.html'),
-        blog: resolve(__dirname, 'blog.html'),
-        'blog-detail': resolve(__dirname, 'blog-detail.html'),
-        faq: resolve(__dirname, 'faq.html')
-      }
-    }
+      input: (() => {
+        const inputs = {
+          main: resolve(__dirname, 'index.html'),
+          about: resolve(__dirname, 'about.html'),
+          events: resolve(__dirname, 'events.html'),
+          partners: resolve(__dirname, 'partners.html'),
+          thankyou: resolve(__dirname, 'thank-you.html'),
+          blog: resolve(__dirname, 'blog.html'),
+          'blog-detail': resolve(__dirname, 'blog-detail.html'),
+          faq: resolve(__dirname, 'faq.html'),
+        }
+
+        // Přidej waitlist stránky jen pokud nejsou v /public
+        addIfExists(inputs, 'coming-soon', resolve(__dirname, 'coming-soon/index.html'))
+        addIfExists(inputs, 'coming-soon-thanks', resolve(__dirname, 'coming-soon/thanks.html'))
+
+        return inputs
+      })(),
+    },
   },
+
   server: {
     proxy: {
       '/api': {
         target: 'http://localhost:8888',
-        changeOrigin: true
-      }
-    }
+        changeOrigin: true,
+      },
+    },
   },
+
   plugins: [
     viteStaticCopy({
       targets: [
-        {
-          src: 'src/locales/*.json',
-          dest: 'locales'
-        }
-      ]
-    })
-  ]
+        { src: 'src/locales/*.json', dest: 'locales' },
+        // Pozn.: cokoliv v /public se kopíruje automaticky
+      ],
+    }),
+  ],
 })
--- webpack.config.js
+++ webpack.config.js
@@ -1,55 +1,97 @@
+// webpack.config.js
 const path = require('path');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const sass = require('sass'); // Dart Sass
 
 module.exports = {
   entry: './src/main.js', // uprav dle potřeby
+
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
-    clean: true
+    clean: true,
+    publicPath: '/', // pro dev server/asset cesty
   },
+
   module: {
     rules: [
       {
         test: /\.scss$/i,
         use: [
           MiniCssExtractPlugin.loader,
-          'css-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              importLoaders: 1,
+              sourceMap: true,
+            },
+          },
           {
             loader: 'sass-loader',
             options: {
+              // používá Dart Sass (@use/@forward)
+              implementation: sass,
+              sourceMap: true,
               additionalData: `
-                @import "@/utils/variables.scss";
-                @import "@/utils/mixins.scss";
+                @use "@styles/utils/variables" as *;
+                @use "@styles/utils/mixins" as *;
               `,
               sassOptions: {
-                includePaths: [path.resolve(__dirname, 'styles')]
-              }
-            }
-          }
-        ]
+                // lze přidat vlastní includePaths, pokud chceš
+                includePaths: [path.resolve(__dirname, 'src/styles')],
+              },
+            },
+          },
+        ],
       },
       {
         test: /\.js$/,
         exclude: /node_modules/,
-        use: ['babel-loader']
-      }
-    ]
+        use: [{
+          loader: 'babel-loader',
+          options: {
+            cacheDirectory: true,
+            presets: [['@babel/preset-env', { targets: 'defaults' }]],
+          },
+        }],
+      },
+      // (volitelné) assets
+      {
+        test: /\.(png|jpe?g|gif|svg|webp)$/i,
+        type: 'asset',
+        parser: { dataUrlCondition: { maxSize: 8 * 1024 } },
+        generator: { filename: 'assets/img/[name][hash][ext]' },
+      },
+      {
+        test: /\.(woff2?|ttf|otf|eot)$/,
+        type: 'asset/resource',
+        generator: { filename: 'assets/fonts/[name][hash][ext]' },
+      },
+    ],
   },
+
   resolve: {
+    extensions: ['.js', '.scss'],
     alias: {
-      '@': path.resolve(__dirname, 'styles')
-    }
+      '@': path.resolve(__dirname, 'src'),
+      '@styles': path.resolve(__dirname, 'src/styles'),
+    },
   },
+
   plugins: [
     new MiniCssExtractPlugin({
-      filename: 'style.css'
-    })
+      filename: 'style.css', // nebo 'assets/css/style.[contenthash].css' pro prod
+    }),
   ],
+
   devServer: {
-    static: './dist',
+    static: { directory: path.resolve(__dirname, 'dist') },
     open: true,
-    port: 3000
+    port: 3000,
+    hot: true,
+    historyApiFallback: true,
   },
-  mode: 'development'
+
+  mode: process.env.NODE_ENV || 'development',
+  devtool: 'source-map',
 };
